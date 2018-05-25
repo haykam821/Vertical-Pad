a68c97e2af7f59355b7f32c9000ed198565b494d
haykam821 <haykam821@users.noreply.github.com>
2018-05-25 18:45:08 -0400

    Complete tests

--- test.js
+++ test.js
@@ -4,19 +4,21 @@ const chai = require("chai");
 const assert = chai.assert;
 
 const testString = "Hello, world!\nThis is a test.";
+const testLines = testString.split("\n").length;
+
 const verticalPad = require("./index.js");
 
 describe("return values with 2-line string", () => {
-	it("stay the same if padding to 0", () => {
-		assert.strictEqual(testString, verticalPad.top(testString, 0));
-		assert.strictEqual(testString, verticalPad.bottom(testString, 0));
+	it("stay the same if padding to same number of lines", () => {
+		assert.strictEqual(testString, verticalPad.top(testString, testLines));
+		assert.strictEqual(testString, verticalPad.bottom(testString, testLines));
 	});
-	it("stay the same if padding to 1", () => {
-	    assert.strictEqual(testString, verticalPad.top(testString, 1));
-	    assert.strictEqual(testString, verticalPad.bottom(testString, 1));
+	it("don't stay the same if padding to more lines than string", () => {
+	    assert.notStrictEqual(testString, verticalPad.top(testString, testLines + 1));
+	    assert.notStrictEqual(testString, verticalPad.bottom(testString, testLines + 1));
 	});
-	it("don't stay the same if padding to longer value than string", () => {
-	    assert.notStrictEqual(testString, verticalPad.top(testString, 3));
-	    assert.notStrictEqual(testString, verticalPad.bottom(testString, 3));
+	it("return a string", () => {
+	    assert.typeOf(verticalPad.top(testString, testLines + 1), "string");
+	    assert.typeOf(verticalPad.bottom(testString, testLines + 1), "string");
 	});
 });
