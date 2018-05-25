1bfc8e6e9bac8b44c8c4c22ad3f0043c41a6a4c4
haykam821 <haykam821@users.noreply.github.com>
2018-05-25 18:39:35 -0400

    Write more tests

--- test.js
+++ test.js
@@ -6,9 +6,17 @@ const assert = chai.assert;
 const testString = "Hello, world!\nThis is a test.";
 const verticalPad = require("./index.js");
 
-describe("the function", () => {
+describe("return values with 2-line string", () => {
 	it("stay the same if padding to 0", () => {
 		assert.strictEqual(testString, verticalPad.top(testString, 0));
 		assert.strictEqual(testString, verticalPad.bottom(testString, 0));
+	});
+	it("stay the same if padding to 1", () => {
+	    assert.strictEqual(testString, verticalPad.top(testString, 1));
+	    assert.strictEqual(testString, verticalPad.bottom(testString, 1));
+	});
+	it("don't stay the same if padding to longer value than string", () => {
+	    assert.notStrictEqual(testString, verticalPad.top(testString, 3));
+	    assert.notStrictEqual(testString, verticalPad.bottom(testString, 3));
 	});
 });
