61b3d6f6b2eeb63d00052e7dceac8f4b681528bb
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-06 14:47:47 -0400

    Attempt addition of padding to middle

--- index.js
+++ index.js
@@ -16,8 +16,26 @@ function bottom(string, lines = 5) {
 		return string + "\n".repeat(needed);
 	}
 }
+function middle(string, lines = 5) {
+	const linesTop = Math.min(lines / 2);
+	const linesBottom = Math.max(lines / 2);
+	
+	const strLines = string.split("\n").length
+	
+	const needed = lines - strLines;
+	const neededTop = linesTop - Math.max(strLines / 2);
+	const neededBottom = linesBottom - Math.min(strLines / 2);
+	
+	if (needed < 0) {
+		// It happens again here.
+		return string;
+	} else {
+		return "\n".repeat(neededTop) + string + "\n".repeat(neededBottom);
+	}
+}
 
 module.exports = {
 	bottom,
+	middle,
 	top,
 };
