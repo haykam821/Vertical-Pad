ba67a7d3b92ae1fa6e46c44b200347fd6416bc32
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-06 14:02:20 -0400

    Fix tests for 1 to 5-line strings

--- test.js
+++ test.js
@@ -5,10 +5,10 @@ const assert = chai.assert;
 
 const testLineData = [
 	"Hello, world!",
-	"Welcome, new galaxy!",
-	"This is\nmulti-line",
-	"This has\nthree\nlines!",
-	"\n\n\n\n\n\n\n\n\n",
+	"Welcome,\nnew galaxy!",
+	"This\nis\nmulti-line",
+	"This\nhas\four\nlines!",
+	"Wow,\nthat\nis\nmany\nlines!",
 ];
 
 const verticalPad = require("./index.js");
@@ -16,7 +16,7 @@ const verticalPad = require("./index.js");
 testLineData.forEach((item, testLines) => {
 	const testString = testLineData.slice(0, testLines).join("\n");
 
-	describe(`return values with ${testLines}-line string`, () => {
+	describe(`return values with ${testLines + 1}-line string`, () => {
 		it("stay the same if padding to same number of lines", () => {
 			assert.strictEqual(testString, verticalPad.top(testString, testLines));
 			assert.strictEqual(testString, verticalPad.bottom(testString, testLines));
