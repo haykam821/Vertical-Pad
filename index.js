82994af64d3e12e4111a122abc32c49827a91618
haykam821 <24855774+haykam821@users.noreply.github.com>
2018-09-06 12:30:16 -0400

    Fix string with more lines than amount to pad to resulting in RangeError

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function padTop(string, lines = 5) {
 	const needed = lines - string.split("\n").length;
-	return "\n".repeat(needed) + string;
+	if (needed < 0) {
+		// We can't pad it, so let's just keep it the same.
+		return string;
+	} else {
+		return "\n".repeat(needed) + string;
+	}
 }
 function padBottom(string, lines = 5) {
 	const needed = lines - string.split("\n").length;
-	return string + "\n".repeat(needed);
+	if (needed < 0) {
+		// The same thing happens here.
+		return string;
+	} else {
+		return string + "\n".repeat(needed);
+	}
 }
 
 module.exports = {
