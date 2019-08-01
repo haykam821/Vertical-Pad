7712d7896f530330c84e5efca7486c3290e517d0
haykam821 <24855774+haykam821@users.noreply.github.com>
2019-08-01 11:20:56 -0400

    Add JSDoc comments

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/**
+ * Pads a string to the top.
+ * @param {string} string The string to pad.
+ * @param {number} lines The number of lines to pad to.
+ * @returns {string} The padded string.
+ */
 function top(string, lines = 5) {
 	const needed = lines - string.split("\n").length;
 	if (needed < 0) {
@@ -7,6 +13,13 @@ function top(string, lines = 5) {
 		return "\n".repeat(needed) + string;
 	}
 }
+
+/**
+ * Pads a string to the bottom.
+ * @param {string} string The string to pad.
+ * @param {number} lines The number of lines to pad to.
+ * @returns {string} The padded string.
+ */
 function bottom(string, lines = 5) {
 	const needed = lines - string.split("\n").length;
 	if (needed < 0) {
@@ -16,16 +29,23 @@ function bottom(string, lines = 5) {
 		return string + "\n".repeat(needed);
 	}
 }
+
+/**
+ * Pads a string to the middle.
+ * @param {string} string The string to pad.
+ * @param {number} lines The number of lines to pad to.
+ * @returns {string} The padded string.
+ */
 function middle(string, lines = 5) {
 	const linesTop = Math.min(lines / 2);
 	const linesBottom = Math.max(lines / 2);
-	
-	const strLines = string.split("\n").length
-	
+
+	const strLines = string.split("\n").length;
+
 	const needed = lines - strLines;
 	const neededTop = linesTop - Math.max(strLines / 2);
 	const neededBottom = linesBottom - Math.min(strLines / 2);
-	
+
 	if (needed < 0) {
 		// It happens again here.
 		return string;
